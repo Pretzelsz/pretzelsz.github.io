3c0fec6b2ffa67faa57d477cdcdfec9aed86db00
Pretzelsz <brownquinn0@gmail.com>
2016-03-10 18:31:17 -0500

    carousell testing

--- js/car.js
+++ js/car.js
@@ -1,23 +1,21 @@
-#
-Rotate event
-rotate = - >
+# Rotate event
+rotate = ->
   $('.card:first-child')
-  .fadeOut 400, 'swing', - >
+    .fadeOut 400, 'swing', ->
 
-  $('.card:first-child')
-  .appendTo('.container')
-  .hide()
+      $('.card:first-child')
+        .appendTo('.container')
+        .hide()
 
-.fadeIn 400, 'swing';
+    .fadeIn 400, 'swing';
 
-#
-Loops through animation
+# Loops through animation
 timeline = setInterval(rotate, 1200)
 
 # Clears timeline on hover
-$('body').hover - >
+$('body').hover ->
   clearInterval(timeline)
 
 # Triggers rotate event on click
-$('.card').click - >
+$('.card').click ->
   rotate()
